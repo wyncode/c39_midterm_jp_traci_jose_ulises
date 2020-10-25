cc4c967c0b98ae9413a8bd547d93fb9f7cf1a276
tracilevine <televine@gmail.com>
2020-10-25 01:51:04 -0400

    pulled code from code sandbox at 1:50 am on Oct 25th

---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Brewery() {
+  return (
+    <div className="navContainer">
+      <img src="#" alt="logo" />
+      <ul className="navbar">
+        <Link to="/Home">
+          <li>Home</li>
+        </Link>
+        <Link to="/AboutUs">
+          <li>About Us</li>
+        </Link>
+        <Link to="/Brewery">
+          <li>Brewery</li>
+        </Link>
+      </ul>
+    </div>
+  );
+}
