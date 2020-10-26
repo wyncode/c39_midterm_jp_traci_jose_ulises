659d1cacebd26f61fea6baea6ec23ed2f4209cbe
purgatorygates <ulisesrivero1984@gmail.com>
2020-10-26 18:16:42 -0400

    new feature mapBox

--- client/src/Pages/BreweryPage.jsx
+++ client/src/Pages/BreweryPage.jsx
@@ -1,49 +1,40 @@
-import React, {useEffect, useState} from "react";
-import {useParams} from 'react-router-dom'
-import {Container} from "react-bootstrap";
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import Map from '../components/Map';
 
 export default function Brewery() {
-
-  const {id} = useParams()
-  const [apiData, setApiData] = useState({})
-
- 
+  const { id } = useParams();
+  const [apiData, setApiData] = useState({});
   useEffect(() => {
-    fetch(`https://api.openbrewerydb.org/breweries/${id}`).then(res => res.json()).then(data => setApiData(data))
-  }, [])
-
-  // console.log("this is coming from brewery page",apiData)
+    fetch(`https://api.openbrewerydb.org/breweries/${id}`)
+      .then((res) => res.json())
+      .then((data) => setApiData(data));
+  }, []);
   return (
     <Container className="breweryBody">
       <div className="brewerytitle">
-  <h1 style={{ margin: "1rem 0" }}>{apiData.name}</h1>
+        <h1 style={{ margin: '1rem 0' }}>{apiData.name}</h1>
       </div>
       {/* <div className="middlebody"> */}
       <div className="brewerypic">
-        <img
-          alt=""
-          src={
-            "https://lh3.ggpht.com/oR0QAL4qQU7zn1wZXGKSrtNJ-Y0UgPSUXpFZCisfnwBhuse3zmYmJLa1R3zGz4SNV9j0FJPajZbrCsutLjASjxefU5L-=s350"
-          }
-        />
+        {apiData.longitude && apiData.latitude && (
+          <Map lng={apiData.longitude} lat={apiData.latitude} />
+        )}
       </div>
       <div className="brewerylist">
         <ul>
-        <li>Phone: {apiData.phone}</li>
-          <li>
-            Address: {apiData.street}
-          </li>
+          <li>Phone: {apiData.phone}</li>
+          <li>Address: {apiData.street}</li>
           <li>
             {apiData.city}, {apiData.state} {apiData.postal_code}
           </li>
           <li>
-            <a href={apiData.website_url} target="_blank"><button className="brewerybtn">Check Out My Website!</button></a>
+            <a href={apiData.website_url} target="_blank">
+              <button className="brewerybtn">Check Out My Website!</button>
+            </a>
           </li>
         </ul>
-      </div>
-      {/* </div> */}
-      <div className="mapbox">
-        <h1>mapbox</h1>
       </div>
     </Container>
   );
