d43acd1e0ab8b5bf61c20896459ad97101d495e8
tracilevine <televine@gmail.com>
2020-10-27 16:31:49 -0400

    SearchBar and  Boxes

--- client/src/components/SearchForm.jsx
+++ client/src/components/SearchForm.jsx
@@ -4,12 +4,15 @@ import Bottle from "./Bottle"
 
 const SearchForm = ({ handleSubmitProp, loading }) => {
   return (
-    <div class="searchContainer" style={{background:'#3357b7', marginBottom:'0px', marginTop:'350px', height:'200px'}}>
-    <Form onSubmit={handleSubmitProp} style={{ margin: '2rem', display:'flex', }}>
+    <div class="searchContainer" style={{background:'#2E2E2E', marginBottom:'0px', marginTop:'350px', height:'200px'}}>
+    <Form onSubmit={handleSubmitProp} style={{ margin: '2rem' }}>
+      <div style={{display:'flex', justifyContent:'space-around'}}>
+      <Form.Row>
+        <h3 style={{color:"#3357b7", fontSize:"70px", fontWeight:"700"}}>Brewery Finder</h3>
+      </Form.Row>
       <Form.Row> 
         <Bottle loading={loading} />
       </Form.Row> 
-      
       <Form.Row >
         <div/>
         <Form.Control
@@ -17,10 +20,11 @@ const SearchForm = ({ handleSubmitProp, loading }) => {
           size="lg"
           type="text"
           placeholder="Enter a City!"
-          style={{background:'transparent', marginTop:"50px", width:'300px'}}
+          style={{background:'transparent', marginTop:"50px", width:'300px', color:'black',border:'none'}}
         >
         </Form.Control>
       </Form.Row>
+      </div>
     </Form>
   </div>
   );
