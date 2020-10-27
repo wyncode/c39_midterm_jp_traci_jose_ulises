8627c1a0d95cd69a18d96cd333ddbf3680b17117
tracilevine <televine@gmail.com>
2020-10-27 04:07:03 -0400

    finally got boxes to alternate, will work on graphic and design

--- client/src/components/BreweryCard.jsx
+++ client/src/components/BreweryCard.jsx
@@ -3,7 +3,6 @@ import {Card} from "react-bootstrap";
 import {Container} from "react-bootstrap";
 import {useHistory} from 'react-router-dom';
 
-
 export default function BreweryCard({brewery, bgColors}) {
   const history = useHistory()
   const handleClick = (id) => {
@@ -12,14 +11,18 @@ export default function BreweryCard({brewery, bgColors}) {
    
 
   return (
+    <div className="card">
+      <h1>{brewery.name}</h1>
+      <h5>{brewery.city}</h5>
+    </div>
   
-    <Container >
-      <Card onClick={() => handleClick(brewery.id)}>
-        <Card.Body style={{width:'100%'}} >
-          <Card.Title className="brewName" style={{backgroundColor:"#3357b7", width:"50%",color:"whitesmoke",fontSize:"30px"}}>{brewery.name}</Card.Title>
-          <Card.Text className="cardText"style={{backgroundColor:"#f2af29", width:"20%",color:"brown", paddingLeft:"10px", height:"30px", fontSize:"20px"}}>{brewery.city}</Card.Text>
-        </Card.Body>
-      </Card>
-    </Container>
+    // <Container >
+    // <Card className="card" onClick={() => handleClick(brewery.id)}>
+    //   <Card.Body style={{width:'100%'}} >
+    //     <Card.Title className="brewName" style={{backgroundColor:"#3357b7", width:"50%",color:"whitesmoke",fontSize:"30px"}}>{brewery.name}</Card.Title>
+    //     <Card.Text className="cardText"style={{backgroundColor:"#f2af29", width:"20%",color:"brown", paddingLeft:"10px", height:"30px", fontSize:"20px"}}>{brewery.city}</Card.Text>
+    //   </Card.Body>
+    // </Card>
+    // </Container>
   );
 }
